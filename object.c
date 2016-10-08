8a2c890d07217047427182148d2c37a2842fad87
Marko Tasic <mtasic85@gmail.com>
2016-10-08 19:18:15 +0200

    func args layout

--- object.c
+++ object.c
@@ -1,7 +1,44 @@
 #include "object.h"
 
-struct l_object_t * l_object_del(struct l_ctx_t * ctx, struct l_object_t * self) {
-    return NULL;
+struct l_object_t * l_object_del(struct l_ctx_t * ctx, struct l_func_args_t args) {
+    struct l_object_t * self;
+
+    switch (args.flag) {
+        case L_FUNC_ARGS_1_ARG:
+            self = args.args.o;
+
+            switch (self->type) {
+                case L_TYPE_NULL:
+                    break;
+                case L_TYPE_BOOL:
+                    break;
+                case L_TYPE_INT:
+                    break;
+                case L_TYPE_FLOAT:
+                    break;
+                case L_TYPE_BYTES:
+                    break;
+                case L_TYPE_STR:
+                    break;
+                case L_TYPE_LIST:
+                    break;
+                case L_TYPE_DICT:
+                    break;
+                case L_TYPE_SET:
+                    break;
+                case L_TYPE_CODE:
+                    break;
+                case L_TYPE_FUNCTION:
+                    break;
+                case L_TYPE_DYNAMIC:
+                    break;
+            }
+
+            break;
+        default:
+            // FIXME: raise exception
+            ;
+    }
 }
 
 struct l_object_t * l_object_is(struct l_ctx_t * ctx, struct l_object_t * a, struct l_object_t * b) {
--- object.h
+++ object.h
@@ -35,19 +35,19 @@ typedef struct l_object_t {
 } l_object_t;
 
 typedef enum l_func_args_flag_t {
-    L_FUNC_ARGS_FLAG_NO_ARGS,
-    L_FUNC_ARGS_FLAG_1_ARG,
-    L_FUNC_ARGS_FLAG_2_ARG,
-    L_FUNC_ARGS_FLAG_3_ARG,
-    L_FUNC_ARGS_FLAG_ARGS,
-    L_FUNC_ARGS_FLAG_KWARGS,
-    L_FUNC_ARGS_FLAG_ARGS_KWARGS,
-    L_FUNC_ARGS_FLAG_CBOOL,
-    L_FUNC_ARGS_FLAG_CINT,
-    L_FUNC_ARGS_FLAG_CFLOAT,
-    L_FUNC_ARGS_FLAG_CCHARP,
-    L_FUNC_ARGS_FLAG_CSTRING,
-    L_FUNC_ARGS_FLAG_CVOIDP
+    L_FUNC_ARGS_NO_ARGS,
+    L_FUNC_ARGS_1_ARG,
+    L_FUNC_ARGS_2_ARG,
+    L_FUNC_ARGS_3_ARG,
+    L_FUNC_ARGS_ARGS,
+    L_FUNC_ARGS_KWARGS,
+    L_FUNC_ARGS_ARGS_KWARGS,
+    L_FUNC_ARGS_CBOOL,
+    L_FUNC_ARGS_CINT,
+    L_FUNC_ARGS_CFLOAT,
+    L_FUNC_ARGS_CCHARP,
+    L_FUNC_ARGS_CSTRING,
+    L_FUNC_ARGS_CVOIDP
 } l_func_args_flag_t;
 
 typedef struct l_func_args_2_args_t {
@@ -73,13 +73,24 @@ typedef union l_func_args_args_t {
     bool b;
     int64_t i;
     double f;
-    char * cc;
-    struct l_cstring_t cs;
-    void * cv;
+    char * c;
+    struct l_cstring_t s;
+    void * v;
 } l_func_args_args_t;
+
+typedef struct l_func_arg_t {
+    struct l_object_t * o;
+    bool b;
+    int64_t i;
+    double f;
+    char * c;
+    struct l_cstring_t s;
+    void * v;
+} l_func_arg_t;
 
 typedef struct l_func_args_t {
     enum l_func_args_flag_t flag;
+    struct l_func_arg_t args[3];
 } l_func_args_t;
 
 #define L_REF(ctx, o) \
@@ -90,13 +101,13 @@ typedef struct l_func_args_t {
 #define L_UNREF(ctx, o) \
     if (o) { \
         o->rc--; \
-        if (o->rc == 0) l_object_del(ctx, o); \
+        if (o->rc == 0) l_object_del(ctx, (l_func_args_t){L_FUNC_ARGS_1_ARG, {o}}); \
     }
 
 #include "ctx.h"
 #include "bool.h"
 
-struct l_object_t * l_object_del(struct l_ctx_t * ctx, struct l_object_t * self);
+struct l_object_t * l_object_del(struct l_ctx_t * ctx, struct l_func_args_t args);
 
 struct l_object_t * l_object_is(struct l_ctx_t * ctx, struct l_object_t * a, struct l_object_t * b);
 struct l_object_t * l_object_eq(struct l_ctx_t * ctx, struct l_object_t * a, struct l_object_t * b);
